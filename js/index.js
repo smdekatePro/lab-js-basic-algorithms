6c94a89a7f22a363c89aa4fb5aedc0a94340771e
Swapnil Dekate <smdekate.pro@gmail.com>
2025-07-08 05:19:15 +0000

    Iteration 1: done

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@
 
 console.log("I'm ready!");
 
+let hacker1 = "lightning"
+console.log(`The driver's name is ${hacker1}`)
+
+let hacker2 = "hollywood"
+console.log(`The navigator's name is ${hacker2}`)
+
 // Iteration 2: Conditionals
 
 
